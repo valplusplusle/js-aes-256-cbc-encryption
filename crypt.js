e5f010ef55beec2ef774537898c16713ddecca3a
Val++le <v.kummert@drmnt.de>
2020-02-26 12:41:14 +0100

    first init

---
+++ crypt.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+var crypto = require('crypto');
+
+var argumentArray = [];
+
+process.argv.forEach((val, index) => {
+    argumentArray[index] = val;
+});
+
+cryptTar(argumentArray[2], argumentArray[3], argumentArray[4])
+
+function cryptTar(pathToFile, pathToSave, key) {   
+    var cipher = crypto.createCipher('aes-256-cbc', key);
+    var input = fs.createReadStream(pathToFile);
+    if (!fs.existsSync(pathToSaveTar)){
+      fs.mkdirSync(pathToSaveTar);
+    }
+    var output = fs.createWriteStream(pathToSave);
+    input.pipe(cipher).pipe(output);
+  
+    output.on('finish', function() {
+      console.log('Encrypted file written to disk!');
+    });
+  }
